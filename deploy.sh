080a420fa85bdb5e922fe1566f918189024ed6c1
SUMITHAA P V R <sumithaapvr.22cse@kongu.edu>
2025-05-11 20:05:28 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u sumithaapvr
-    docker tag test1 sumithaapvr/agri
+    docker tag agriconnect sumithaapvr/agri
     docker push sumithaapvr/agri
     docker-compose up -d
     
